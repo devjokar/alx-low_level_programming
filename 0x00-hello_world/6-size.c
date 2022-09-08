277348145bdb3a00674026e9f33ede52eee6efde
Jo Kar <josi.kelechi@gmail.com>
2022-09-08 13:38:26 -0700

    change size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,9 @@
 
 /**
  * main - Entry point
- * @void: empty
  * Return: 0 always success
  */
-int main(void0)
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
