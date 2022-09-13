5b8b46228899caa8e8b041bfef10e3493cd21ec6
Jo Kar <josi.kelechi@gmail.com>
2022-09-13 16:31:03 -0700

    Enter 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Calculate the absolute value
+ * @n: integer argument
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0 )
+		return (n);
+	else
+		return (i * -1);
+}
+
