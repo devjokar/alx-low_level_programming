f47d0ee73676a5b183acd9c742a46f695d2b9716
Jo Kar <josi.kelechi@gmail.com>
2022-09-13 16:22:10 -0700

    Enter 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Ascertain if input is 
+ * an alphabet
+ * @a: integer argument
+ *
+ * Return: 1 for letters, 0 otherwise
+ */
+int _isalpha(int a)
+{
+	if ((a >96 && a < 123) || (a > 64 && a < 91))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
+
