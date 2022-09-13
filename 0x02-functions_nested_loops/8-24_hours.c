ea465a0b6339decc9a980293b72003e9aeba6b99
Jo Kar <josi.kelechi@gmail.com>
2022-09-13 16:45:06 -0700

    Enter 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Display minutes of a day
+ */
+void jack_bauer(void)
+{
+	int c, d;
+
+	c = 0;
+	while (c < 24)
+	{
+		d = 0;
+		while (d < 60)
+		{
+			if (c < 10)
+			{
+				_putchar(48);
+				_putchar(48 + c % 10);
+			}
+			else
+			{
+				_putchar(48 + c / 10);
+				_putchar(48 + c % 10);
+			}
+			_putchar(':');
+			if (d < 10)
+			{
+				_putchar(48);
+				_putchar(48 + d);
+			}
+			else
+			{
+				_putchar(48 + d / 10);
+				_putchar(48 + d % 10);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
