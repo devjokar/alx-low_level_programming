22767e4088f85d587a2673d7620fe48531218277
Jo Kar <josi.kelechi@gmail.com>
2022-09-13 17:33:42 -0700

    Refactor style to betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,18 @@
  */
 void print_to_98(int n)
 {
-	for (; n > 98; n--)
-		printf("%d, ", n);
-	for (; n < 98; n++)
-		printf("%d, ", n);
-	printf("%d\n, ", 98);
+	if (n == 98)
+	{
+		printf("98\n");
+		return;
+	}
+	else
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+		for (; n < 98; n++)
+			printf("%d, ", n);
+		printf("98\n");
+	}
 }
 
