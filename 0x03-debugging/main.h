0222960157aa7e171873cabbcf776e4ae25d7778
Jo Kar <josi.kelechi@gmail.com>
2022-09-15 16:56:55 -0700

    Attach all files

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+void positive_or_negative(int c);
+int largest_number(int a, int b, int c);
+int convert_day(int m, int d);
+void print_remaining_days(int m, int d, int y);
+
+#endif
