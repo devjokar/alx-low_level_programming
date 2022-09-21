016ab3e1e4ebfd969cb7af1f74fb2dc3f04d87c4
Jo Kar <josi.kelechi@gmail.com>
2022-09-21 05:58:41 -0700

    Create all project files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - combune two strings
+ * @dest: destination steing arg
+ * @src: source string arg
+ * Return: pointer to combined string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
+
