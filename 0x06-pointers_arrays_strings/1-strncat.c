0ff2f27f1b41c784935559f4fb5899cef30b7494
Jo Kar <josi.kelechi@gmail.com>
2022-09-22 02:45:48 -0700

    Change two files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,2 +1,26 @@
 #include "main.h"
 
+/**
+ * _strncat - combine two strings with limit n
+ * @dest: destination
+ * @src: source
+ * @n: bytes from src
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (j < n)
+	{
+		dest[i] = src[j];
+		if (src[j] == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
+
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,2 +1,26 @@
 #include "main.h"
 
+/**
+ * _strncpy - copy a given string
+ * @dest: destination
+ * @src: source
+ * @n: bytes from src
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
