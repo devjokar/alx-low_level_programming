16ae0d2b2db70c925175b940544c19bba577e461
Jo Kar <josi.kelechi@gmail.com>
2022-09-22 02:57:07 -0700

    Change 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,2 +1,22 @@
 #include "main.h"
 
+/**
+ * reverse_array - reverse array contents
+ * @a: array
+ * @n: array length
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	i = 0;
+	while (i < n / 2)
+	{
+		j = n - i - 1;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+	}
+}
+
