a1424438a6f6c48cca3bdc86e5980c9bc15d2b4d
Jo Kar <josi.kelechi@gmail.com>
2022-09-22 03:18:35 -0700

    Change 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,2 +1,32 @@
 #include "main.h"
 
+/**
+ * leet - encode a string in leet
+ * @s: input string
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	int lower[] = {97, 101, 111, 116, 108};
+	int upper[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (j < 5)
+		{
+			if (s[i] == lower[j] || s[i] == upper[j])
+			{
+				s[i] = num[i];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
+
