c35e42160a5c9161112356a9f31eeaca5d7b57d1
Jo Kar <josi.kelechi@gmail.com>
2022-09-22 15:36:29 -0700

    debug 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	i = j = 0;
 	while (j == 0)
 	{
-		if ((si[i] == '\0') && (s2[i] == '\0'))
+		if ((s1[i] == '\0') && (s2[i] == '\0'))
 			break;
 		j = s1[i] - s2[i];
 		i++;
