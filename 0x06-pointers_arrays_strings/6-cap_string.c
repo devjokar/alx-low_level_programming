f8fd8c94d838bdb2b00c5c4d099250fc838904e1
Jo Kar <josi.kelechi@gmail.com>
2022-09-22 03:11:31 -0700

    Change 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,2 +1,33 @@
 #include "main.h"
 
+/**
+ * cap_string - capitalizes words in a string
+ * @s: input string
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] -= 32;
+		j = 0;
+		while (j < 13)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] -= 32;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
+
