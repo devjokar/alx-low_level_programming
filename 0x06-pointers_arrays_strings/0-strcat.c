fc0a2ac5f484e0b53569de3dbb4d041a2053e787
Jo Kar <josi.kelechi@gmail.com>
2022-09-22 01:54:58 -0700

    Complete 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] >= 0)
+	{
+		dest[i] = src[j];
+		if (src[j] == '\0');
+			break;
+		i++, j++;
+	}
 	return (dest);
 }
 
