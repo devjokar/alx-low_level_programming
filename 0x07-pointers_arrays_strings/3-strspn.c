0a53c46831f7af5ecdda5992caa3a3c585ed91fd
Jo Kar <josi.kelechi@gmail.com>
2022-09-27 17:26:01 -0700

    Chnage files 3 & 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,32 @@
 #include "main.h"
 
 /**
+ * _strspn - gets the length of a prefix substring
+ * @s: initial str segment
+ * @accept: accepted bytes
+ * Return: Number of accepted bytes
  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, n;
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		n = 1;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				n = 0;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (n == 1)
+		break;
+	return (i);
+}
 
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
 /**
+ * _strpbrk - searches a steing for any of a set of byyes
+ * @s: first string
+ * @accept: second string
+ * Return: Pointer to bytes in s
  */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s[i]);
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
 
