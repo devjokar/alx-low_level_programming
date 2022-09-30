d6d5282065de9976df1e509142248127902fbd79
Jo Kar <josi.kelechi@gmail.com>
2022-09-30 14:38:17 -0700

    Complete files 2 and 3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
@@ -9,7 +8,12 @@
  * Return: always 0
  */
 int main(int argc, char *argv[])
-}
+{
+	int i;
+
+	if (argc > 0)
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
 	return (0);
 }
 
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
@@ -9,7 +8,19 @@
  * Return: always 0
  */
 int main(int argc, char *argv[])
-}
+{
+	int prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		prod = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", prod);
+	}
 	return (0);
 }
 
