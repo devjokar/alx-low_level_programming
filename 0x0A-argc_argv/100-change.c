bec1da64fabd414d6baa39698dfae75966f431f1
Jo Kar <josi.kelechi@gmail.com>
2022-09-30 14:43:23 -0700

    Complete 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
@@ -9,7 +8,38 @@
  * Return: always 0
  */
 int main(int argc, char *argv[])
-}
+{
+	int coins, i, amt;
+	int value[5] = {25, 10, 5, 2, 1};
+
+	coins = 0;
+	amt = atoi(argv[argc - 1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (amt <= 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (value[i] <= amt)
+			{
+				coins += (amt / value[i]);
+				amt -= (amt / value[i]) * value[i];
+				if (amt == 0)
+				{
+					printf("%d\n", coins);
+					break;
+				}
+			}
+		}
+	}
 	return (0);
 }
 
