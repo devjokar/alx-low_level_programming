4aa6b608f7708ec5f8851c8ea7ca8e25250887c4
Jo Kar <josi.kelechi@gmail.com>
2022-10-04 14:45:32 -0700

    Debug 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 	else if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
