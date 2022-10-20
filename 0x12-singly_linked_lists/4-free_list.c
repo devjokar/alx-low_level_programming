665852d73460543fdfd12d3346186eb9e11f6443
Jo Kar <josi.kelechi@gmail.com>
2022-10-20 15:23:26 -0700

    Complete project code

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 
+/**
+ * free_list - free memory of all nodes in list
+ * @head: pointer to head in fucntion
+ */
+void free_list(list_t *head)
+{
+	list_t *curr_node, *next_node;
 
+	curr_node = head;
+	while (curr_node != NULL)
+	{
+		next_node = curr_node;
+		free(curr_node->str);
+		free(curr_node);
+		curr_node = next_node->next;
+	}
+}
