e7bf44a245115924c685f62c14dba0dd6b495b23
Josi Karachi <josi.kelechi@gmail.com>
2023-04-14 01:25:00 +0100

    Edit 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,8 @@
 #include "search_algos.h"
 
 /**
- * _rsearch - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int _rsearch(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (!array || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (_rsearch(array, half, value));
-
-	half++;
-	return (_rsearch(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
+ * binary_search - search for index of a value using
+ * the binary search algorithm
  *
  * @array: input array
  * @size: size of the array
@@ -47,11 +11,26 @@ int _rsearch(int *array, size_t size, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+	size_t i, left = 0, right = size - 1;
 
-	index = _rsearch(array, size, value);
-	if (index >= 0 && array[index] != value)
+	if (!array)
 		return (-1);
 
-	return (index);
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
